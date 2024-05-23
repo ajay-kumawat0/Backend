6a0746977871ea18a50f14fd1066eb4ae06cc7be
ajay-kumawat0 <ajay@elemensissoftech.com>
2024-05-23 11:23:48 +0530

    api response handling

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const DBConnect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(DBConnect.connection.host); // connection.host =>only show production connection
+        // console.log(DBConnect.connection.host); // connection.host =>only show production connection
     } catch (err) {
         console.log('DB not connected', err);
         process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
-dotenv.config({ path: './env' })
-connectDB();
+dotenv.config({ path: './env' });
+import app from './app.js'
 
-
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, (err) => {
+            err ? console.log(err) : console.log(`Server is connected on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("DB connection failed", err);
+    })
 
 
 
